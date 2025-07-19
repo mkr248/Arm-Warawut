b32793b8c4a5d606fbd9d95a55c5d50129a9c38f
mkr248 <hanginthere1992@gmail.com>
2025-07-20 01:20:17 +0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 console.log("Hello, world!"); // Logs a string
 let myVariable = 10;
 console.log("The value of myVariable is:", myVariable); // Logs a string and a variable
-let myObject = { name: "Alice", age: 30 , banking : 1000000000};
+let myObject = { name: "warawut", age: 33, banking : 1000000000};
 console.log(myObject); // Logs an object
 console.log(myObject); // Logs an object
 ('This would be the main JS file.');
